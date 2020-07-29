003ed5d2af4ac9286fba57b0244e6fba17d39614
Pornchai <john64115@gmail.com>
2020-07-29 15:55:49 +0700

    Data and Basic input

---
+++ src/Lab2/BasicInput.java
@@ -0,0 +1,26 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class BasicInput {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter you name ? : ");
+        String name = scanner.nextLine();
+        System.out.println("Your name is "+name);
+
+        System.out.print("อายุเท่าไร ?: ");
+        int age = scanner.nextInt();
+        System.out.println("อายุของคุณคือ " + age);
+
+        System.out.print("Enter you Nickname ? : ");
+        String Nickname = scanner.nextLine();
+        System.out.println("Your name is : "+name);
+
+
+    }//main
+
+}//main
---
+++ src/exercise/Chapter4_1.java
@@ -0,0 +1,8 @@
+package exercise;
+
+public class Chapter4_1 {
+    public static void main(String[] args) {
+
+    }
+
+}
