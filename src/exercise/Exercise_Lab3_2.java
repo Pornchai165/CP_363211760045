fcd2e2674447b8c3991a895dd638a1632f11f861
Pornchai <john64115@gmail.com>
2020-08-19 15:49:51 +0700

    .3

--- src/exercise/Exercise_Lab3_2.java
+++ src/exercise/Exercise_Lab3_2.java
@@ -6,7 +6,7 @@ public class Exercise_Lab3_2 {
 
         for (int i = 1; i <=10; i++) {
             for (int j = 1; j <=i; j++) {
-                System.out.print("*");
+                System.out.print(j);
 
             }//j
             System.out.println();
