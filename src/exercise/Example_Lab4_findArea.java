06f3b45fae0c2903d92530c1e3aaa6fc076fef7a
Pornchai <john64115@gmail.com>
2020-08-26 15:55:35 +0700

    Login

---
+++ src/exercise/Example_Lab4_findArea.java
@@ -0,0 +1,15 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class Example_Lab4_findArea {
+    Scanner Scanner = new Scanner(System.in);
+
+
+    public static int findArea(int x,int y,int z) {
+        return (x < y) ? x : y;
+
+
+
+    }
+}
