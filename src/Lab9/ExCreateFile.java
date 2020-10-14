8aa3ed1baa5775bfc894e600b1bbdb3b528b1af3
STP_COM08 <john64115@gmail.com>
2020-10-14 14:57:04 +0700

    exception Lab 8 and exercise

--- src/Lab9/ExCreateFile.java
+++ src/Lab9/ExCreateFile.java
@@ -17,8 +17,35 @@ public class ExCreateFile {
             e.printStackTrace();
         }
 
+        if (myFile.canRead()) {
+            System.out.println("Can read this file.");
+        } else {
+            System.out.println("can not read this file.");
+        }
+        //canWrite()
+        if (myFile.canWrite()) {
+            System.out.println("Can Write this file.");
+        } else {
+            System.out.println("can not Write this file.");
+        }
+        //exists
+        if (myFile.exists()) {
+            System.out.println("This file is exists");
+        } else {
+            System.out.println("This file is not exists");
+        }
 
-
+        //getName()
+        System.out.println("File name is "+myFile.getName());
+        //getPath()
+        System.out.println("Path file is "+myFile.getPath());
+        //rendNameTo()
+        File rFile = new File("myNewFile.txt");
+        if (myFile.renameTo(rFile)) {
+            System.out.println("File name is already changed");
+        } else {
+            System.out.println("Can not change file name");
+        }
 
 
     }
